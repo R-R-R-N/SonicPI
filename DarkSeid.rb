d544276e9a0d91317ad08e33c8c99eff3bffdc68
R-R-R-N <124717478+R-R-R-N@users.noreply.github.com>
2023-02-14 11:32:00 -0800

    Create DarkSeid.rb

---
+++ DarkSeid.rb
@@ -0,0 +1,49 @@
+#Darkseid Intro
+sample :ambi_choir, beat_stretch:3
+sleep 1
+sample :ambi_dark_woosh, amp:1
+3.times do
+  play 30
+  sleep 0.5
+end
+3.times do
+  play 30
+  sleep 0.25
+end
+3.times do
+  play 40
+  sleep 0.25
+end
+play 70
+3.times do
+  play 30
+  sleep 0.5
+end
+3.times do
+  play 30
+  sleep 0.25
+end
+3.times do
+  play 40
+  sleep 0.25
+end
+play 70
+sleep 1
+3.times do
+  play 40
+  sleep 0.25
+end
+sleep 0.25
+play 70
+sleep 0.5
+3.times do
+  play 40
+  sleep 0.25
+end
+sleep 0.25
+play 70
+sleep 0.25
+5.times do
+  play 30
+  sleep 0.25
+end
