053de976736f1d1ca4f677e045a52b0216f8dea0
R-R-R-N <124717478+R-R-R-N@users.noreply.github.com>
2023-02-14 11:32:38 -0800

    Create SevenNationArmy.rb

---
+++ SevenNationArmy.rb
@@ -0,0 +1,17 @@
+# Seven Nation Army
+use_bpm 120
+
+play :e3
+sleep 1.5
+play :e3
+sleep 0.5
+play :g3
+sleep 0.75
+play :e3
+sleep 0.25
+sleep 0.5
+play :d3
+sleep 0.5
+play :c3
+sleep 2
+play :b2
