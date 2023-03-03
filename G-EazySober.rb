c31b8533ad1e7998be67b06e9ba3f3946ace66e3
R-R-R-N <124717478+R-R-R-N@users.noreply.github.com>
2023-03-03 11:33:25 -0800

    Update G-EazySober.rb

--- G-EazySober.rb
+++ G-EazySober.rb
@@ -1,55 +1,67 @@
 # Sober
 
+x = 0.25
+y = 0.5
+z = 0.75
+
 use_bpm 96
 use_synth :beep
 
+sober_intro = "C:/Users/Ryan Ramos Noriega/Documents/Audacity/G-Eazy_-_Sober_Instrumental2.wav"
+sample sober_intro
+
+#intro
+sleep 28
+play :e5
+sleep x
+play :fs5
+sleep y
+play :a5
+sleep x
+
 #measure 1
-play :e5
-sleep 0.25
-play :fs5
-sleep 0.5
-play :a5
-sleep 0.25
+live_loop :Regret_this_when_its_over do
+  sleep 1
+  play :fs5
+  sleep x
+  play :fs5
+  sleep z
+  play :e5
+  sleep x
+  play :fs5
+  sleep y
+  play :cs5
+  sleep x
+  play :cs5
+  sleep x
+  play :b4
+  sleep z
+  play :b4
+  sleep x
+  play :a4
+  sleep y
+  play :cs5
+  sleep x
+  
+  #measure 2
+  play :cs5
+  sleep x
+  play :a4
+  sleep x
+  play :b4
+  sleep y
+  play :b4
+  sleep y
+  play :cs5
+  sleep y
+  sleep z
+  play :e5
+  sleep x
+  play :e5
+  sleep x
+  play :fs5
+  sleep y
+  play :a5
+  sleep x
+end
 
-#measure 2
-play :fs5
-sleep 0.25
-play :fs5
-sleep 0.75
-play :e5
-sleep 0.25
-play :fs5
-sleep 0.5
-play :cs5
-sleep 0.25
-play :cs5
-sleep 0.25
-play :b4
-sleep 0.75
-play :b4
-sleep 0.25
-play :a4
-sleep 0.5
-play :cs5
-sleep 0.25
-
-#measure 3
-play :cs5
-sleep 0.25
-play :a4
-sleep 0.25
-play :b4
-sleep 0.5
-play :b4
-sleep 0.5
-play :cs5
-sleep 0.5
-sleep 0.75
-play :e5
-sleep 0.25
-play :e5
-sleep 0.25
-play :fs5
-sleep 0.5
-play :a5
-sleep 0.25
