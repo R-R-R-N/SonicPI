23d1abd45e2110c7c29dca374a3c406c72053c8b
R-R-R-N <124717478+R-R-R-N@users.noreply.github.com>
2023-03-10 10:25:22 -0800

    Update SevenNationArmy.rb

--- SevenNationArmy.rb
+++ SevenNationArmy.rb
@@ -1,17 +1,24 @@
-# Seven Nation Army
-use_bpm 120
+# Seven Nation Army by The White Stripes
 
-play :e3
-sleep 1.5
-play :e3
-sleep 0.5
-play :g3
-sleep 0.75
-play :e3
-sleep 0.25
-sleep 0.5
-play :d3
-sleep 0.5
-play :c3
-sleep 2
-play :b2
+use_bpm 120
+use_synth :fm
+use_synth_defaults sustain: 0.25
+
+live_loop :white_stripes do
+  play :e3
+  sleep 1.5
+  play :e3
+  sleep 0.5
+  play :g3
+  sleep 0.75
+  play :e3
+  sleep 0.25
+  play :r   # this is a "rest"
+  sleep 0.5
+  play :d3
+  sleep 0.5
+  play :c3
+  sleep 2
+  play :b2
+  sleep 2
+end
