4683f6f24deaa1203fafbe3d1b79a45b397e7e24
R-R-R-N <124717478+R-R-R-N@users.noreply.github.com>
2023-03-30 21:50:49 -0700

    Update G-EazyHimAndI2.rb

--- G-EazyHimAndI2.rb
+++ G-EazyHimAndI2.rb
@@ -1,67 +1,71 @@
 # Him and I: G-Eazy
 use_bpm 88
 s = HimAndI_Intro = "C:/Users/Ryan Ramos Noriega/Downloads/G-Eazy and Halsey - Him & I (Official Instrumental).wav"
+ss = HimAndI_Outro = "C:/Users/Ryan Ramos Noriega/Downloads/G-Eazy & Halsey - Him & I (Lyrics).wav"
 
+sample :ambi_choir, beat_stretch:6
+sample :ambi_choir, beat_stretch:10
+
+sleep 10
 sample (s)
 
-33.times do
-  play :b4
-  sleep 0.475
-  play :d4
-  sleep 0.475
-end
-
+sleep 32
 live_loop :notes do
-  #Measure 1
-  play :c4
-  sleep 0.25
-  sleep 0.5
-  play :d4
-  sleep 0.5
-  sleep 0.25
-  play :d4
-  sleep 0.25
-  #Measure 2
-  play :e4
-  sleep 1
-  play :c4
-  sleep 0.5
-  sleep 0.5
-  play :d4
-  sleep 0.25
-  sleep 0.25
-  play :d4
-  sleep 0.25
-  play :e4
-  sleep 1
-  play :c4
-  sleep 0.25
-  sleep 0.5
-  play :d4
-  sleep 0.5
-  sleep 0.25
-  play :d4
-  sleep 0.25
-  #Measure 3
-  play :d4
-  sleep 0.25
-  sleep 0.5
-  play :a3
-  sleep 0.5
-  sleep 0.25
-  play :a3
-  sleep 0.25
-  play :c4
-  sleep 0.25
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  sleep 0.25
-  play :b4
-  sleep 0.25
-  play :b4
-  sleep 0.25
-  sleep 0.5
-  play :a3
-  sleep 0.75
+  4.times do
+    #Measure 1
+    play :c4
+    sleep 0.25
+    sleep 0.5
+    play :d4
+    sleep 0.5
+    sleep 0.25
+    play :d4
+    sleep 0.25
+    #Measure 2
+    play :e4
+    sleep 1
+    play :c4
+    sleep 0.5
+    sleep 0.5
+    play :d4
+    sleep 0.25
+    sleep 0.25
+    play :d4
+    sleep 0.25
+    play :e4
+    sleep 1
+    play :c4
+    sleep 0.25
+    sleep 0.5
+    play :d4
+    sleep 0.5
+    sleep 0.25
+    play :d4
+    sleep 0.25
+    #Measure 3
+    play :d4
+    sleep 0.25
+    sleep 0.5
+    play :a3
+    sleep 0.5
+    sleep 0.25
+    play :a3
+    sleep 0.25
+    play :c4
+    sleep 0.25
+    sleep 0.5
+    play :b4
+    sleep 0.5
+    sleep 0.25
+    play :b4
+    sleep 0.25
+    play :b4
+    sleep 0.25
+    sleep 0.5
+    play :a3
+    sleep 0.75
+  end
+  stop
 end
+sleep 49.5
+sample (ss)
