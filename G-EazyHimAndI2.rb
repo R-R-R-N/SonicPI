75e8ff563af3d957736f82586d6700179f60078c
R-R-R-N <124717478+R-R-R-N@users.noreply.github.com>
2023-03-30 17:38:30 -0700

    Create G-EazyHimAndI2.rb

---
+++ G-EazyHimAndI2.rb
@@ -0,0 +1,67 @@
+# Him and I: G-Eazy
+use_bpm 88
+s = HimAndI_Intro = "C:/Users/Ryan Ramos Noriega/Downloads/G-Eazy and Halsey - Him & I (Official Instrumental).wav"
+
+sample (s)
+
+33.times do
+  play :b4
+  sleep 0.475
+  play :d4
+  sleep 0.475
+end
+
+live_loop :notes do
+  #Measure 1
+  play :c4
+  sleep 0.25
+  sleep 0.5
+  play :d4
+  sleep 0.5
+  sleep 0.25
+  play :d4
+  sleep 0.25
+  #Measure 2
+  play :e4
+  sleep 1
+  play :c4
+  sleep 0.5
+  sleep 0.5
+  play :d4
+  sleep 0.25
+  sleep 0.25
+  play :d4
+  sleep 0.25
+  play :e4
+  sleep 1
+  play :c4
+  sleep 0.25
+  sleep 0.5
+  play :d4
+  sleep 0.5
+  sleep 0.25
+  play :d4
+  sleep 0.25
+  #Measure 3
+  play :d4
+  sleep 0.25
+  sleep 0.5
+  play :a3
+  sleep 0.5
+  sleep 0.25
+  play :a3
+  sleep 0.25
+  play :c4
+  sleep 0.25
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  sleep 0.25
+  play :b4
+  sleep 0.25
+  play :b4
+  sleep 0.25
+  sleep 0.5
+  play :a3
+  sleep 0.75
+end
