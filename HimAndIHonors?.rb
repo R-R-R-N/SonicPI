382613a3ce46ddbfd74897d625f2f9845dc4db2c
R-R-R-N <124717478+R-R-R-N@users.noreply.github.com>
2023-04-14 11:04:20 -0700

    Update HimAndIHonors?.rb

--- HimAndIHonors?.rb
+++ HimAndIHonors?.rb
@@ -3,60 +3,71 @@ use_bpm 88
 s = HimAndI_Intro = "C:/Users/Ryan Ramos Noriega/Documents/Audacity/Intro2.wav"
 ss = HimAndI_Outro = "C:/Users/Ryan Ramos Noriega/Documents/Audacity/Vocals.wav"
 a = 1
+tempo = [0.75, 0.75, 0.25, 1, 1, 0.5, 0.25, 1, 0.75, 0.75, 0.25, 0.75, 0.75, 0.25, 0.75, 0.75, 0.25, 0.75, 0.75]
+t = 0
 
-define :music do |n|
+define :music do |n, e|
   #Measure 1
   play :c4
-  sleep 0.25
-  sleep 0.5
+  sleep tempo [t]
+  t = t + 1
   play :d4
-  sleep 0.5
-  sleep 0.25
+  sleep tempo [t]
+  t = t + 1
   play :d4
-  sleep 0.25
+  sleep tempo [t]
+  t = t + 1
   #Measure 2
-  play :e4
-  sleep 1
+  play e
+  sleep tempo [t]
+  t = t + 1
   play :c4
-  sleep 0.5
-  sleep 0.5
+  sleep tempo [t]
+  t = t + 1
   play :d4
-  sleep 0.25
-  sleep 0.25
+  sleep tempo [t]
+  t = t + 1
   play :d4
-  sleep 0.25
-  play :e4
-  sleep 1
+  sleep tempo [t]
+  t = t + 1
+  play e
+  sleep tempo [t]
+  t = t + 1
   play :c4
-  sleep 0.25
-  sleep 0.5
+  sleep tempo [t]
+  t = t + 1
   play :d4
-  sleep 0.5
-  sleep 0.25
+  sleep tempo [t]
+  t = t + 1
   play :d4
-  sleep 0.25
+  sleep tempo [t]
+  t = t + 1
   #Measure 3
   play :d4
-  sleep 0.25
-  sleep 0.5
+  sleep tempo [t]
+  t = t + 1
   play n
-  sleep 0.5
-  sleep 0.25
+  sleep tempo [t]
+  t = t + 1
   play n
-  sleep 0.25
+  sleep tempo [t]
+  t = t + 1
   play :c4
-  sleep 0.25
-  sleep 0.5
+  sleep tempo [t]
+  t = t + 1
   play :b4
-  sleep 0.5
-  sleep 0.25
+  sleep tempo [t]
+  t = t + 1
   play :b4
-  sleep 0.25
+  sleep tempo [t]
+  t = t + 1
   play :b4
-  sleep 0.25
-  sleep 0.5
+  sleep tempo [t]
+  t = t + 1
   play n
-  sleep 0.75
+  sleep tempo [t]
+  t = t + 1
+  t = 0
 end
 
 define :function do
@@ -78,7 +89,7 @@ sample (s)
 sleep 32
 live_loop :notes do
   4.times do
-    music :a3
+    music :a3, :e4
   end
   stop
 end
